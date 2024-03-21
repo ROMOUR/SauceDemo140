cfeeefc5d6b9f15b0ee3866c758b961e5e0f82b9
RMOUR <rodolfo_itg@yahoo.com.br>
2024-03-20 21:25:46 -0300

    Inicio cenario outline

--- features/steps/selecionar_produtos.py
+++ features/steps/selecionar_produtos.py
@@ -3,6 +3,7 @@ from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+@given(u'que entro no site Sauce Demo')
 @given(u'que acesso o site Sauce Demo')
 def step_impl(context):
     # Setup / Inicialização
@@ -20,7 +21,7 @@ def step_impl(context, usuario, senha):
     context.driver.find_element(By.ID, "login-button").click()          # clicar no botão login
    
 
-@then(u'sou direcionado para página Home')
+@then(u'exibe mensagem de erro no login')
 def step_impl(context):
     assert context.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"
     # time.sleep(2) # espera por 2 segundos - remover depois = alfinete
@@ -28,3 +29,10 @@ def step_impl(context):
     # teardown / encerramento
     context.driver.quit()
 
+@then(u'exibe a mensagem de erro no login')
+def step_impl(context):
+   # validar a mensagem de erro
+    assert context.driver.find_element(By.CSS_SELECTOR, "h3").text == "Epic sadface: Username and password do not match any user in this service"
+ 
+    # teardown / encerramento
+    context.driver.quit()
